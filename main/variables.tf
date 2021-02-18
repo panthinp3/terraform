8d7688e585a3d3b33e6ceeb5cdb64e167c52b19f
Mamata Khatri <mamatakhatri@Mamatas-MacBook-Air.local>
2021-02-18 12:22:11 -0600

    Removed access key and secret key

--- main/variables.tf
+++ main/variables.tf
@@ -1,14 +1,5 @@
 variable "region"{
     type = string
     description= "Enter the region: "
+    default = "us-east-1"
 }
-variable "access_key"{
-    type = string
-    description= "Enter your access key: "
-    
-}
-variable "secret_key"{
-    type = string
-    description= "Enter your secret key: "
-}
-
